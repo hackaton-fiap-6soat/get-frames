eed3263d181a47c58d33cf871ca00142b267763f
Ingrid Queiroz <queirozingrid.c@gmail.com>
2025-01-21 19:00:50 -0300

    Fix deploy problems

--- terraform/main.tf
+++ terraform/main.tf
@@ -87,7 +87,7 @@ resource "aws_api_gateway_resource" "upload_resource" {
 # Method GET on /frames/upload
 resource "aws_api_gateway_method" "get_method" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.upload-resource.id
+  resource_id   = aws_api_gateway_resource.upload_resource.id
   http_method   = "POST"
   authorization = "NONE"
 }
@@ -96,7 +96,7 @@ resource "aws_api_gateway_method" "get_method" {
 resource "aws_api_gateway_integration" "lambda_integration_video" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.upload_resource.id
-  http_method             = aws_api_gateway_method.post_method_cpf.http_method
+  http_method             = aws_api_gateway_method.get_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.application_entry.invoke_arn
@@ -115,7 +115,7 @@ resource "aws_api_gateway_integration" "lambda_integration_video" {
 # API Gateway Integration with Lambda (/frames/upload)
 resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.upload-resource.id
+  resource_id             = aws_api_gateway_resource.upload_resource.id
   http_method             = aws_api_gateway_method.get_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
